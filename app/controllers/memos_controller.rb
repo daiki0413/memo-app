f72ce29b342a5523e07e42551cd8db4828ae1d76
EC2 Default User <ec2-user@ip-172-31-17-58.us-east-2.compute.internal>
2021-02-04 08:26:18 +0000

    add delete and edit

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -2,6 +2,7 @@ class MemosController < ApplicationController
     def index
         #トップページが呼ばれたときに動作するアクション
         @memos=Memo.all
+        
     end
     
     def new
@@ -11,6 +12,25 @@ class MemosController < ApplicationController
     def create
         #新しいメモがフォームからpostされたときに動作するアクション
         Memo.create(title:params["memos"]["title"],body:params["memos"]["body"])
-        redirect_to "/"
+        redirect_to 
+        "/"
     end    
+    
+    def delete
+        memo=Memo.find(params["id"])
+        memo.destroy
+        redirect_to "/"
+    end
+    
+    def edit
+        @memo=Memo.find(params["id"])
+    end
+    
+    def update
+        memo=Memo.find(params["id"])
+        memo.title=params["memos"]["title"]
+        memo.body=params["memos"]["body"]
+        memo.save
+        redirect_to"/"
+    end
 end
